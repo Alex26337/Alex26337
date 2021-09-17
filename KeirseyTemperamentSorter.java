5ce8912ea7cc48701834fc352113a8356fe85909
Alex26337 <89935213+Alex26337@users.noreply.github.com>
2021-09-16 22:08:45 -0500

    Update KeirseyTemperamentSorter.java

--- KeirseyTemperamentSorter.java
+++ KeirseyTemperamentSorter.java
@@ -12,7 +12,7 @@ public class KeirseyTemperamentSorter{
     public String encode(String name, String[] list){
         
         
-        return String.format("");
+        return String.format(name + "");
     }
     
 }
