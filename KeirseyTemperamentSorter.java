7caebf5c4ba0c0e91a43a47bde34ec9f7b7613a4
Alex26337 <89935213+Alex26337@users.noreply.github.com>
2021-09-17 12:48:28 -0500

    Update KeirseyTemperamentSorter.java

--- KeirseyTemperamentSorter.java
+++ KeirseyTemperamentSorter.java
@@ -19,9 +19,29 @@ public class KeirseyTemperamentSorter{
         
         for(int i = 0; i<list.length;i++){
             if(i%7==0){
-                if(list[i].toLowerCase().equals("a"))aCount[1]++;
-                else if(list[i].toLowerCase().equals("b"))bCount[1]++;
+                switch(list[i].toLowerCase()){
+                    case "a" : aCount[0]++; break;
+                    case "b" : bCount[0]++; break;
+                }
+            }else if(i%7==1||i%7==2){
+                switch(list[i].toLowerCase()){
+                    case "a" : aCount[1]++; break;
+                    case "b" : bCount[1]++; break;
+                }
+            }else if(i%7==3||i%7==4){
+                switch(list[i].toLowerCase()){
+                    case "a" : aCount[2]++; break;
+                    case "b" : bCount[2]++; break;
+                }
+            }else if(i%7==5||i%7==6){
+                switch(list[i].toLowerCase()){
+                    case "a" : aCount[3]++; break;
+                    case "b" : bCount[3]++; break;
+                }
             }
+                
+        }for(int a = 0; i<bPercent.length; a++){
+            bPercent[a] = (bCount[a]*100.0)/(bCount[a]+aCount[a]);
         }
         
         return String.format(name + "\n");
