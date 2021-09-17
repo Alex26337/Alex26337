c56615bcc297005259f7f143de91def19d345010
Alex26337 <alexcromans@gmail.com>
2021-09-16 22:02:03 -0500

    Create KeirseyTemperamentSorter.java

---
+++ KeirseyTemperamentSorter.java
@@ -0,0 +1,18 @@
+/*
+ *  Authors: Alexander Romans, Derly Buentello
+ *  Date: September 16, 2021
+ *  Class: Computer Science II K
+ *  Name: KeirseyTemperamentSorter.java
+ */
+
+public class KeirseyTemperamentSorter{
+    
+    public String[] recordedAnswers = new String[70];
+    
+    public String encode(String name, String[] list){
+        
+        
+        return String.format("");
+    }
+    
+}
