4f411e2bd297e0eed8ce868ee91f732656f7a16f
Alex26337 <alexcromans@gmail.com>
2021-09-19 01:43:03 -0500

    BIG Additions and Testing

--- KeirseyTemperamentSorter.java
+++ KeirseyTemperamentSorter.java
@@ -5,6 +5,10 @@
  *  Name: KeirseyTemperamentSorter.java
  */
 
+import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class KeirseyTemperamentSorter{
@@ -12,15 +16,15 @@ public class KeirseyTemperamentSorter{
     private String[] recordedAnswers = new String[70];
     
     private static String percentConversion(int[] list){
-        String output = "";
+        
         for(int i=0; i<list.length;i++){
             switch(i){
-                case 0: output+= list[i]>=50?"I":"E"; break;
-                case 1: output+= list[i]>=50?"N":"S"; break;
-                case 2: output+= list[i]>=50?"F":"T"; break;
-                case 3: output+= list[i]>=50?"P":"J"; break;
+                case 0: return list[i]==100||list[i]>=50&&list[i]<100?"I":list[i]>=0&&list[i]<50?"E":"-";
+                case 1: return list[i]==100||list[i]>=50&&list[i]<100?"N":list[i]>=0&&list[i]<50?"S":"-";
+                case 2: return list[i]==100||list[i]>=50&&list[i]<100?"F":list[i]>=0&&list[i]<50?"T":"-";
+                case 3: return list[i]==100||list[i]>=50&&list[i]<100?"P":list[i]>=0&&list[i]<50?"J":"-";
             }    
-        }return output;
+        }return "----";
     }
     public static String encode(String name, String[] list){
         
@@ -57,12 +61,24 @@ public class KeirseyTemperamentSorter{
             bPercent[a] = (int)((bCount[a]*100.0)/(bCount[a]+aCount[a]));
         }
         
-        return String.format(name + ": \n%dA-%dB %dA-%dB %dA-%dB %dA-%dB\n[%d%%, %d%%, %d%%, %d%%] = "+percentConversion(bPercent), aCount[0], bCount[0], aCount[1], bCount[1], aCount[2], bCount[2], aCount[3], bCount[3], bPercent[0], bPercent[1], bPercent[2], bPercent[3]);
+        return String.format(name + ": \n%dA-%dB %dA-%dB %dA-%dB %dA-%dB\n[%d%%, %d%%, %d%%, %d%%] = "+percentConversion(bPercent)+"\n\n", aCount[0], bCount[0], aCount[1], bCount[1], aCount[2], bCount[2], aCount[3], bCount[3], bPercent[0], bPercent[1], bPercent[2], bPercent[3]);
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         
-        Scanner kb = new Scanner(System.in);
+        File output = new File("output.txt");
+        Scanner data = new Scanner(new File("E:\\Other\\GitMode\\src\\personality.txt"));
+        PrintWriter out = new PrintWriter(new FileWriter(output));
+        String name;
+        while(data.hasNextLine()){
+            name = data.nextLine();
+            //System.out.println("Initialized Name");
+            if(data.hasNextLine())out.println(encode(name, data.nextLine().split("")));
+            //System.out.println("Encoded Scantron");
+        }
+        Scanner outRead = new Scanner(output);
+        while(outRead.hasNextLine())System.out.println(outRead.nextLine());
+        out.close();
         
     }
     
